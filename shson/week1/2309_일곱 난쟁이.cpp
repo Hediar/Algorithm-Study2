57a5171140cb3fd8cda95abc5013a02c03a2d03a
sonshn <sonshn@naver.com>
2023-08-27 14:07:04 +0900

    Week1: 2309_일곱 난쟁이, 16926_배열 돌리기 1

---
+++ shson/week1/2309_일곱 난쟁이.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	int arr[9] = { 0, };
+	int sum = 0;
+
+	for (int i = 0; i < 9; i++) {
+		cin >> arr[i];
+		sum += arr[i];
+	}
+
+	sort(arr, arr + 9);
+	
+	for (int i = 0; i < 8; i++) {
+		for (int j = 1; j < 9; j++) {
+			if (sum - (arr[i] + arr[j]) == 100) {
+
+				for (int k = 0; k < 9; k++) {
+					if (k == i || k == j) continue;
+
+					cout << arr[k] << '\n';
+				}
+
+			}
+		}
+	}
+
+	return 0;
+}
