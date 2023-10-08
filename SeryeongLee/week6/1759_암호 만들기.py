eff966db9b483e6272e52f8353ac2c0b547e1b6b
Seryeong Lee <72387948+Hediar@users.noreply.github.com>
2023-10-08 16:09:10 +0900

    10/8 1105

--- SeryeongLee/week6/1759_암호 만들기.py
+++ SeryeongLee/week6/1759_암호 만들기.py
@@ -1,3 +1,10 @@
 # https://deeply-silence-9a4.notion.site/1759-d0b9bc64f49444b9bddc2b88327ef4bd?pvs=4
 import sys
 input = sys.stdin.readline
+
+vowel = ['a', 'e', 'i', 'o', 'u']
+L, C = map(int, input().split())
+words = input().split()
+words.sort()
+
+
